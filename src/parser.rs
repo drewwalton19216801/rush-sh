62d22304d4bf09395fa6c3600e625725b814d886
Drew Walton <drewwalton19216801@gmail.com>
2026-01-11 01:48:21 -0700

    Handle RedirOutClobber in all redir loops

--- src/parser.rs
+++ src/parser.rs
@@ -925,6 +925,15 @@ fn parse_commands_sequentially(tokens: &[Token]) -> Result<Ast, String> {
                             }
                         }
                     }
+                    Token::RedirOutClobber => {
+                        i += 1;
+                        if i < tokens.len() {
+                            if let Token::Word(file) = &tokens[i] {
+                                redirections.push(Redirection::OutputClobber(file.clone()));
+                                i += 1;
+                            }
+                        }
+                    }
                     Token::RedirIn => {
                         i += 1;
                         if i < tokens.len() {
@@ -1362,6 +1371,17 @@ fn parse_pipeline(tokens: &[Token]) -> Result<Ast, String> {
                                     current_cmd
                                         .redirections
                                         .push(Redirection::Output(file.clone()));
+                                    i += 1;
+                                }
+                            }
+                        }
+                        Token::RedirOutClobber => {
+                            i += 1;
+                            if i < tokens.len() {
+                                if let Token::Word(file) = &tokens[i] {
+                                    current_cmd
+                                        .redirections
+                                        .push(Redirection::OutputClobber(file.clone()));
                                     i += 1;
                                 }
                             }
